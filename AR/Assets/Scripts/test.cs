bd7b52d59d044cc4b6b0deb5c09ee7fb009a4f4e
christiankoning <christiankoning001@fclive.nl>
2019-10-25 15:33:16 +0200

    Update test.cs
    Removed old testing code.
    Added a bit of information before making the wheel collider testing script

--- AR/Assets/Scripts/test.cs
+++ AR/Assets/Scripts/test.cs
@@ -4,120 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-[RequireComponent(typeof(ARRaycastManager))]
 public class test : MonoBehaviour
 {
-    ///
-    ///Reflection Probes: Shows real time reflection of the physical world
-    ///Problems: 
-    ///How to use it?
-    ///What are the limitations?
-    ///How to call the reflecting material to become a texture on the carpaint?
-    ///
-    ///
-    ///
-    ///Reflection probes would make the carpaint material and the model look more realistic.
-    ///To make the scaling and placing the object better check through the arfoundation-samples project
-    ///arfoundation-samples project has some interesting scenes with some helpful tools. Might be worth looking through to get better quality in ar
-
-    public GameObject PlacedPrefab;
-    private GameObject spawnedObject;
-    private ARRaycastManager m_RaycastManager;
-    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
-    private bool moveDevice = true;
-    private bool tapToPlace = false;
-    public GameObject moveDeviceObject;
-    public GameObject tapToPlaceObject;
-    private bool isSpawned = false;
-    public GameObject canvas;
-    public ARPlaneManager m_PlaneManager;
-
-    ARPlaneManager planemanager
-    {
-        // Checks the amount of planes that have been placed (keeps it on 0 if there are no planes found (yet))
-        get { return m_PlaneManager; }
-        set { m_PlaneManager = value; }
-    }
-    bool TryGetTouchPosition(out Vector3 touchPosition)
-    {
-#if UNITY_EDITOR
-        if (Input.GetMouseButton(0))
-        {
-            var mousePosition = Input.mousePosition;
-            touchPosition = new Vector3(mousePosition.x, mousePosition.y, mousePosition.z);
-            return true;
-        }
-#else
-        if(Input.touchCount > 0)
-        {
-            touchPosition = Input.GetTouch(0).position;
-            return true;
-        }
-#endif
-        touchPosition = default;
-        return false;
-    }
-    void Awake()
-    {
-        //Searches for the object that contains the ARRaycastManager component
-        m_RaycastManager = GetComponent<ARRaycastManager>();
-    }
-    void Update()
-    {
-        AnimationChecker();
-        CheckUI();
-        if (!TryGetTouchPosition(out Vector3 touchPosition))
-        {
-            return;
-        }
-        if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
-        {
-            var hitPose = s_Hits[0].pose;
-            if (spawnedObject == null)
-            {
-                spawnedObject = Instantiate(PlacedPrefab, hitPose.position, hitPose.rotation);
-                isSpawned = true;
-                foreach (var plane in planemanager.trackables)
-                {
-                    plane.gameObject.SetActive(false);
-                }
-                m_PlaneManager.enabled = false;
-            }
-        }
-    }
-    bool PlanesFound()
-    {
-        if (planemanager == null)
-        {
-            return false;
-        }
-        return planemanager.trackables.count > 0;
-    }
-    void AnimationChecker()
-    {
-        if (PlanesFound() && moveDevice)
-        {
-            moveDeviceObject.SetActive(false);
-            moveDevice = false;
-            tapToPlaceObject.SetActive(true);
-            tapToPlace = true;
-        }
-        if (isSpawned && tapToPlace)
-        {
-            tapToPlaceObject.SetActive(false);
-            tapToPlace = false;
-        }
-    }
-    void CheckUI()
-    {
-        if (isSpawned)
-        {
-            canvas.SetActive(true);
-        }
-        else
-        {
-            canvas.SetActive(false);
-        }
-    }
-
+    ///With the use of wheel colliders the car model should be able to drive
+    ///The wheels should be able to make a turn and it needs to look realistic
+    ///So the parent of the tires should be rotated towards the direction you want to drive
+    ///The wheel should roll like its actually driving and not sliding to its destination
+    ///Maybe add car sounds for a more realistic experience?
+    ///The steering wheel should rotate aswell if you want to turn
+    ///Maybe make it possible to click on the doors and it will open/close
+    ///The car should not be able to flip. To prevent that from happening there needs to be a speed limit
+    ///The car should also be heavy enough to make it unable to flip
+    ///The car needs to be able to drive forward and in reverse. The car needs breaks aswell
 }
