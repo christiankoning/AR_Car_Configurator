9e67a1fbbafebf8a9fae66448601489016122dfb
christiankoning <christiankoning001@fclive.nl>
2019-10-30 11:02:39 +0100

    Edited Scenes + test script
    Reactivated bmw model in model scene.
    Changed size of image in test scene.
    Removed the description about drivable car in test scene.

--- AR/Assets/Scripts/test.cs
+++ AR/Assets/Scripts/test.cs
@@ -6,17 +6,5 @@ using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 public class test : MonoBehaviour
 {
-    ///With the use of wheel colliders the car model should be able to drive
-    ///The wheels should be able to make a turn and it needs to look realistic
-    ///So the parent of the tires should be rotated towards the direction you want to drive
-    ///The wheel should roll like its actually driving and not sliding to its destination
-    ///Maybe add car sounds for a more realistic experience?
-    ///The steering wheel should rotate aswell if you want to turn
-    ///Maybe make it possible to click on the doors and it will open/close
-    ///The car should not be able to flip. To prevent that from happening there needs to be a speed limit
-    ///The car should also be heavy enough to make it unable to flip
-    ///The car needs to be able to drive forward and in reverse. The car needs breaks aswell
-    ///In the UI top right corner get 2 images:
-    ///A steeringwheel that when pressed gives the UI to drive and turns off the modifications options in the UI
-    ///A modification image that turns off the driving abilities and turns on the modification options.
+
 }
